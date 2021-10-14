699e4270f32f69e32b1ae3f64a72443c6aa87988
kristian240 <kristian.djakovic@infinum.hr>
2021-10-14 13:04:00 +0200

    (react-components) setup

---
+++ packages/react-components/dist/components.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+import { defineCustomElements } from 'core-components/loader';
+defineCustomElements();
+export const CoreSidebar = /*@__PURE__*/ createReactComponent('core-sidebar');
+export const MyComponent = /*@__PURE__*/ createReactComponent('my-component');
+//# sourceMappingURL=components.js.map
---
+++ packages/react-components/src/components.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'core-components';
+
+import { defineCustomElements } from 'core-components/loader';
+
+defineCustomElements();
+export const CoreSidebar = /*@__PURE__*/createReactComponent<JSX.CoreSidebar, HTMLCoreSidebarElement>('core-sidebar');
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
