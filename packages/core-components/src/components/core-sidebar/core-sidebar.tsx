7d17739f7ac93d1cd0d4076ec9ac0739c2537849
kristian240 <kristian.djakovic@infinum.hr>
2021-10-14 12:28:34 +0200

    (core) create sidebar component

---
+++ packages/core-components/src/components/core-sidebar/core-sidebar.tsx
@@ -0,0 +1,67 @@
+import { Component, Host, h, Element, Prop, Method, State } from '@stencil/core';
+
+@Component({
+  tag: 'core-sidebar',
+  styleUrl: 'core-sidebar.css',
+  shadow: true,
+})
+export class CoreSidebar {
+  static _id = 0;
+  @Element()
+  el!: HTMLCoreSidebarElement;
+
+  /**
+   * Prop to define if sidebar is expanded or not
+   */
+  @Prop({
+    mutable: true,
+    reflect: true,
+  })
+  expanded: boolean = false;
+
+  @Method()
+  toggle() {
+    this.expanded = !this.expanded;
+  }
+
+  @Method()
+  expand() {
+    this.expanded = true;
+  }
+
+  @Method()
+  collapse() {
+    this.expanded = false;
+  }
+
+  @State()
+  counter = 0;
+
+  componentWillLoad() {
+    this.el.id ||= `sidebar-${CoreSidebar._id++}`;
+  }
+
+  render() {
+    const sidebarHeaderEl = this.el.querySelector('[slot=header]');
+    const sidebarHeaderId = sidebarHeaderEl?.id || `${this.el.id}-header`;
+
+    const sidebarContentEl = this.el.querySelector('[slot=content]');
+    const sidebarContentId = sidebarContentEl?.id || `${this.el.id}-content`;
+
+    return (
+      <Host aria-labelledby={sidebarHeaderId} aria-describedby={sidebarContentId} aria-expanded={this.expanded}>
+        <slot name="header"></slot>
+        <slot name="content"></slot>
+
+        <div style={{ display: 'flex' }}>
+          <button style={{ marginRight: '4px' }} onClick={() => (this.counter += 1)}>
+            +
+          </button>
+          <pre>{this.counter}</pre>
+        </div>
+
+        <slot name="footer"></slot>
+      </Host>
+    );
+  }
+}
